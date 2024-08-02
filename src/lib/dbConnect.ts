6707b82feeeb0e2396d59d7b601e0baf45c17d1c
NjG19o2 <njg19o2@gmail.com>
2024-08-02 20:27:13 +0530

    db connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+type connectionObject = {
+    isConnected?: number;
+}
+
+const connection : connectionObject ={};
+
+async function dbConnect() : Promise<void> {
+    if(connection.isConnected){
+        console.log("Already connected");
+        return;
+    }
+
+    try {
+        const db= await mongoose.connect(process.env.MONGODB_URI || '',{})
+
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("Connected to MongoDB");
+    } catch (error) {
+
+        console.log("Failed to connect", error);
+
+        process.exit(1);
+        
+    }
+}
+
+export default dbConnect;
